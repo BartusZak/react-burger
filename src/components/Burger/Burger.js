3b254901913ef9bf05d6fd8519891377bc52ad3e
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-19 21:58:35 +0100

    114. Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    //tworze funkcje       //obiekt o ilości pól tyle ile prop.ingredients ma kluczy    
+    const transformedIngredients = Object.keys(props.ingredients)
+        // wykonuje operacje na kazdym elemencie po kluczu (ktory nazwe igKey)
+        .map(igKey => { 
+        //zwracam tablice spread operator (dopisuje do tablcy  klucze i wykonuje funkcje z parametrem blank)
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    });
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,26 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    //Old syntax
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    //new syntax
+    state = {
+        ingredients : {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return(
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>
                     Build Control
                 </div>
