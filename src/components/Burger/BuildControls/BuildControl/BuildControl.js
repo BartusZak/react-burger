f5e1f9bd0d6a1ea1dd3ecb6a3b4596992925d5c7
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-20 12:40:36 +0100

    119. Removing Ingredients Safely

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,13 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>Mniej</button>
-        <button className={classes.More} onClick={props.added}>Więcej</button>
+        <button 
+            className={classes.Less}
+            onClick={props.removed}
+            disabled={props.disabled}>Mniej</button>
+        <button
+            className={classes.More}
+            onClick={props.added}>Więcej</button>
     </div>
 );
 
