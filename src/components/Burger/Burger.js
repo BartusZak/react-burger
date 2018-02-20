90f87959097964e1173e5473e890f627a1ef8028
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2018-02-20 11:06:38 +0100

    115. Calculating the Ingredient Sum Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,14 +6,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
 
     //tworze funkcje       //obiekt o ilości pól tyle ile prop.ingredients ma kluczy    
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         // wykonuje operacje na kazdym elemencie po kluczu (ktory nazwe igKey)
         .map(igKey => { 
         //zwracam tablice spread operator (dopisuje do tablcy  klucze i wykonuje funkcje z parametrem blank)
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />
         });
-    });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Proszę dodać składniki.</p>;
+    }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
